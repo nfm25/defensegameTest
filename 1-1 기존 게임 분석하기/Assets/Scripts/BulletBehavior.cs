b31431e6c9425e72c1eea63c29bbaad1e73de4e1
nfm25 <49427856+nfm25@users.noreply.github.com>
2019-04-10 13:30:31 +0900

    총알 발사 관련 소스코드 개선

--- 1-1 기존 게임 분석하기/Assets/Scripts/BulletBehavior.cs
+++ 1-1 기존 게임 분석하기/Assets/Scripts/BulletBehavior.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour {
 
-    public float speed = 10.0f;
+    public BulletStat bulletStat { get; set; } // get; set; 다음에 생성자가 온다. 그러므로 아래 public BulletBehavior 부분이 생성자임.
+
+    public BulletBehavior() // BulletBehavior가 초기화 될때, bulletStat 또한 0, 0으로(스피드, 데미지) 초기화되도록 설정한다.
+    {
+        bulletStat = new BulletStat(0, 0);
+    }
     public GameObject character;
 
-    private int damage;
+    
 
-    public void setDamage(int input)
-    {
-        damage = input;
-    }
-
+   
 	void Start () {
         Destroy(gameObject, 3.0f);
 	}
 	
 	void Update () {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,7 +28,7 @@ public class BulletBehavior : MonoBehaviour {
         if(other.gameObject.tag == "Monster")
         {
             Destroy(gameObject);
-            other.GetComponent<MonsterStat>().attacked(damage);
+            other.GetComponent<MonsterStat>().attacked(bulletStat.damage);
         }   
     }
 }
---
+++ 1-1 기존 게임 분석하기/Assets/Scripts/BulletStat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStat //VO ==> Value Object, 단순히 다른 스크립트에 값을 부여하기 위한 데이터만 저장, 설정하는 스크립트
+    // 그러므로, MonoBehaviour가 필요하지 않다.
+{
+    public float speed { get; set; }
+    public int damage { get; set; }
+
+    public BulletStat(float speed, int damage)
+    {
+        this.speed = speed; // this ==> 생성자
+        this.damage = damage;
+    }
+}
+  
